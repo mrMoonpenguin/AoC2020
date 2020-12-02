bba157b20b4604d030b2f8e6102fd687710ba60a
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-02 11:20:13 +0100

    finished part 2 of day2

--- day2/day2.hs
+++ day2/day2.hs
@@ -5,7 +5,7 @@ main :: IO ()
 main = do
   contents <- readFile "inputDay2.txt"
   let input = lines contents
-  print $ check $ parseInput input
+  print $ check2 $ parseInput input
 
 readInt :: String -> Int
 readInt = read
@@ -28,6 +28,20 @@ f l = n >= mi && n <= ma
     c = head (l !! 2)
     s = l !! 3
     n = length (filter (== c) s)
+
+check2 :: [[String]] -> Int
+check2 lst = length $ filter g lst
+
+g :: [String] -> Bool
+g l = n < length s && m < length s && xor (s !! n == c) (s !! m == c)
+  where
+    n = readInt (head l) - 1
+    m = readInt (l !! 1) - 1
+    c = head (l !! 2)
+    s = l !! 3
+
+xor :: Bool -> Bool -> Bool
+xor a b = (a && (not b)) || ((not a) && b)
 
 -- check :: [[String]] -> Int
 -- check lst = length $ filter f lst
