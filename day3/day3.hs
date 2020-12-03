82b888c7ab473345a1abeacf80c1d65f8a19652b
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-03 10:43:03 +0100

    cleaned up, added a cleaner general solution

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,25 +1,26 @@
+-- -- part 1
+-- main :: IO ()
+-- main =
+--   do
+--     contents <- readFile "inputDay3.txt"
+--     let input = lines contents
+--     print $ countTrees input 0 0 3 1
+
+-- part 2
 main :: IO ()
 main =
   do
     contents <- readFile "inputDay3.txt"
     let input = lines contents
-    print ((countTrees input 0 1 0) * (countTrees input 0 3 0) * (countTrees input 0 5 0) * (countTrees input 0 7 0) * (countTrees' input 0 1 0))
+    print $ countTrees input 0 0 1 1 * countTrees input 0 0 3 1 * countTrees input 0 0 5 1 * countTrees input 0 0 7 1 * countTrees input 0 0 1 2
 
--- delta_y = 1
-countTrees :: [String] -> Int -> Int -> Int -> Int
-countTrees [] _ _ acc = acc
-countTrees (s : ss) x d acc
-  | s !! x == '#' = countTrees ss x' d (acc + 1)
-  | otherwise = countTrees ss x' d acc
+-- general solution
+countTrees :: [String] -> Int -> Int -> Int -> Int -> Int
+countTrees lst x y dx dy
+  | y >= l = 0
+  | otherwise = n + countTrees lst x' y' dx dy
   where
-    x' = mod (x + d) (length s)
-
--- delta_y = 2
-countTrees' :: [String] -> Int -> Int -> Int -> Int
-countTrees' [] _ _ acc = acc
-countTrees' (s : []) _ _ acc = acc
-countTrees' (s1 : s2 : ss) x d acc
-  | s1 !! x == '#' = countTrees' ss x' d (acc + 1)
-  | otherwise = countTrees' ss x' d acc
-  where
-    x' = mod (x + d) (length s1)
+    l = length lst
+    x' = mod (x + dx) (length (head lst))
+    y' = y + dy
+    n = if (lst !! y) !! x == '#' then 1 else 0
