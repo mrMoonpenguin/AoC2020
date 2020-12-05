471606da5746105e59af63d114adf933d7b79244
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-05 11:43:04 +0100

    day5: cleaned up parser 😎

--- day5/day5.hs
+++ day5/day5.hs
@@ -1,5 +1,3 @@
-import Data.Bits
-
 main :: IO ()
 main =
   do
@@ -7,15 +5,10 @@ main =
     print . findSeat . parseInput . lines $ contents
 
 parseInput :: [String] -> [Int]
-parseInput lst = [r * 8 + c | s <- lst, let r = binSearch (take 7 s) 0 127, let c = binSearch (drop 7 s) 0 7]
+parseInput lst = [r * 8 + c | s <- lst, let s' = map (\x -> if x == 'B' || x == 'R' then 1 else 0) (reverse s), let r = binToDec $ drop 3 s', let c = binToDec $ take 3 s']
 
-binSearch :: String -> Int -> Int -> Int
-binSearch [] m n = m
-binSearch (s : ss) m n
-  | s == 'F' || s == 'L' = binSearch ss m (n - x)
-  | s == 'B' || s == 'R' = binSearch ss (m + x) n
-  where
-    x = div (n - m + 1) 2
+binToDec :: [Int] -> Int
+binToDec = foldr (\x y -> x + 2 * y) 0
 
 findSeat :: [Int] -> Int
 findSeat lst = (+ 1) $ head $ filter (\x -> notElem (x + 1) lst && elem (x + 2) lst) lst
