ae2b82b0e500f1d4777ee671e002c21b220bf82e
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-06 10:37:53 +0100

    day6: finished part 2, pretty clean again, I'm getting good at this 😎

--- day6/day6.hs
+++ day6/day6.hs
@@ -2,8 +2,19 @@ import Data.List
 import Data.List.Split
 import Data.List.Utils (replace)
 
+-- -- part 1
+-- main :: IO ()
+-- main =
+--   do
+--     contents <- readFile "inputDay6.txt"
+--     print . foldr ((\x y -> length x + y) . nub . replace "\n" "") 0 . splitOn "\n\n" $ contents
+
+-- part 2
 main :: IO ()
 main =
   do
     contents <- readFile "inputDay6.txt"
-    print . foldr ((\x y -> length x + y) . nub . replace "\n" "") 0 . splitOn "\n\n" $ contents
+    print . foldr ((\x y -> length x + y) . intersect' . words) 0 . splitOn "\n\n" $ contents
+
+intersect' :: Eq a => [[a]] -> [a]
+intersect' lst = foldl intersect (head lst) lst
