3000f54fa6cb1a34a881a485115f5ac472ae8297
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-06 12:48:18 +0100

    day6: a bit of cleanup 😎

--- day6/day6.hs
+++ day6/day6.hs
@@ -7,14 +7,11 @@ import Data.List.Utils (replace)
 -- main =
 --   do
 --     contents <- readFile "inputDay6.txt"
---     print . foldr ((\x y -> length x + y) . nub . replace "\n" "") 0 . splitOn "\n\n" $ contents
+--     print . foldr ((+) . length . nub . replace "\n" "") 0 . splitOn "\n\n" $ contents
 
 -- part 2
 main :: IO ()
 main =
   do
     contents <- readFile "inputDay6.txt"
-    print . foldr ((\x y -> length x + y) . intersect' . words) 0 . splitOn "\n\n" $ contents
-
-intersect' :: Eq a => [[a]] -> [a]
-intersect' lst = foldl intersect (head lst) lst
+    print . foldr ((+) . length . foldr1 intersect . words) 0 . splitOn "\n\n" $ contents
