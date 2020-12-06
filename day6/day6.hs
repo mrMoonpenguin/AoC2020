fa3bcdfc597636c5cee622cf99a5cbb7a39433ab
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-06 12:50:57 +0100

    day6: changed both parts to oneliners 😎

--- day6/day6.hs
+++ day6/day6.hs
@@ -4,14 +4,8 @@ import Data.List.Utils (replace)
 
 -- -- part 1
 -- main :: IO ()
--- main =
---   do
---     contents <- readFile "inputDay6.txt"
---     print . foldr ((+) . length . nub . replace "\n" "") 0 . splitOn "\n\n" $ contents
+-- main = readFile "inputDay6.txt" >>= print . foldr ((+) . length . nub . replace "\n" "") 0 . splitOn "\n\n"
 
 -- part 2
 main :: IO ()
-main =
-  do
-    contents <- readFile "inputDay6.txt"
-    print . foldr ((+) . length . foldr1 intersect . words) 0 . splitOn "\n\n" $ contents
+main = readFile "inputDay6.txt" >>= print . foldr ((+) . length . foldr1 intersect . words) 0 . splitOn "\n\n"
