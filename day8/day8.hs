2d531e12e2b4d09e2faede33fa44443187e2df7b
mrMoonpenguin <dulger.hasbi@gmail.com>
2020-12-08 10:38:17 +0100

    day8: finished part 2, not too bad. Again, should have used a data structure with constant lookup

--- day8/day8.hs
+++ day8/day8.hs
@@ -6,18 +6,35 @@ data Operation = Op {fn :: String, arg :: Int} deriving (Show)
 main :: IO ()
 main = do
   contents <- readFile "inputDay8.txt"
-  print . run 0 0 empty . map ((\x -> Op (x !! 0) (readInt $ x !! 1)) . words) . lines . replace "+" "" $ contents
+  print . run 0 0 empty . findBadLine 0 empty . map ((\x -> Op (x !! 0) (readInt $ x !! 1)) . words) . lines . replace "+" "" $ contents
 
 readInt :: String -> Int
 readInt = read
 
 run :: Int -> Int -> Set Int -> [Operation] -> Int
+run acc _ _ [] = acc
 run acc pos set ops
+  | pos >= length ops = acc
   | member pos set = acc
   | otherwise = case instr of
     "acc" -> run (acc + arg op) (pos + 1) (insert pos set) ops
     "jmp" -> run acc (pos + arg op) (insert pos set) ops
     "nop" -> run acc (pos + 1) (insert pos set) ops
+  where
+    op = ops !! pos
+    instr = fn op
+
+findBadLine :: Int -> Set Int -> [Operation] -> [Operation]
+findBadLine pos set ops = case instr of
+  "acc" -> findBadLine (pos + 1) (insert pos set) ops
+  "jmp" ->
+    if member (pos + arg op) set
+      then take pos ops ++ [Op "nop" (arg op)] ++ drop (pos + 1) ops
+      else findBadLine (pos + arg op) (insert pos set) ops
+  "nop" ->
+    if member (pos + 1) set
+      then take pos ops ++ [Op "jmp" (arg op)] ++ drop (pos + 1) ops
+      else findBadLine (pos + 1) (insert pos set) ops
   where
     op = ops !! pos
     instr = fn op
